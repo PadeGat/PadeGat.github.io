f8cd70f7928fb6457dc59e0595c8fa0cdf678cc4
Samantha <exp.samantha@gmail.com>
2023-12-22 15:17:08 +0100

    files

---
+++ index.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById('drawingCanvas');
+    const ctx = canvas.getContext('2d');
+
+    let currentColor = 'black';
+    let lineWidth = 5;
+
+    let isDrawing = false;
+
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', stopDrawing);
+    canvas.addEventListener('mouseout', stopDrawing);
+
+    function startDrawing(e) {
+      isDrawing = true;
+      draw(e);
+    }
+    
+    function draw(e) {
+      if (!isDrawing) return;
+    
+      ctx.strokeStyle = currentColor;
+      ctx.lineWidth = lineWidth;
+      ctx.lineCap = 'round';
+    
+      ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    }
+    
+    function stopDrawing() {
+      isDrawing = false;
+      ctx.beginPath(); 
+    }
